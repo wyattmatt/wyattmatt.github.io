43723782292b575f535a0708c83747864d62a1c8
Wyatt Matthew <wyatt.honny06@gmail.com>
2025-01-20 16:50:10 +0800

    Update main.js
    pagebreak:    { mode: ['avoid-all', 'css', 'legacy'] }

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,7 +105,7 @@ let opt = {
     image:        { type: 'jpeg', quality: 0.98 },
     html2canvas:  { scale: 4 },
     jsPDF:        { format: 'a4', orientation: 'portrait' },
-    pagebreak:    { mode: 'avoid-all', before: '#page2el' }
+    pagebreak:    { mode: ['avoid-all', 'css', 'legacy'] }
 }
 
 //Function to call areaCV and Html2Pdf options
